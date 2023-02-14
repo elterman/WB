273013f38072630176efc4fd8ba8fe9f13ca3d9a
Arkady Elterman <elterman@gmail.com>
2023-02-14 13:46:43 -0800

    .

--- src/Targets Base Page.jsx
+++ src/Targets Base Page.jsx
@@ -1,15 +1,20 @@
 import { useAtomValue } from 'jotai';
-import { a_grid_data, a_theme } from './atoms';
+import { a_grid_data, a_selected_tab, a_theme } from './atoms';
 import HiGrid from './HiGrid';
 import _ from 'lodash';
 import { formatNumeric } from './utils';
+import { COMPARE } from './const';
 
 const TargetsBasePage = (props) => {
     const { columnHeaders, readOnly, isCellEditable, onAcceptChange, createNode, canSave, onSave } = props;
     const { nodes } = useAtomValue(a_grid_data);
     const theme = useAtomValue(a_theme);
+    const tab = useAtomValue(a_selected_tab);
 
     const sectionSize = columnHeaders?.length;
+    const compare = tab === COMPARE;
+
+    const isnot = (value, test) => formatNumeric(value) !== formatNumeric(test);
 
     const getCellStyle = (node, col) => {
         const style = {};
@@ -18,8 +23,14 @@ const TargetsBasePage = (props) => {
         const values = node.data;
         const value = values[col];
 
-        if (section === 3 && level === 1 && formatNumeric(value) !== '100.0') {
-            style.background = theme.warn;
+        if (section === 3 && level === 1) {
+            if (compare) {
+                if (col === sectionSize * 3 && isnot(value, 0)) {
+                    style.background = theme.warn;
+                }
+            } else if (isnot(value, 100)) {
+                style.background = theme.warn;
+            }
         } else if ((section === 2 && !node.children && +value) || (section === 3 && +values[col - sectionSize])) {
             style.background = theme.change;
         }
@@ -30,9 +41,9 @@ const TargetsBasePage = (props) => {
     const pnode = nodes?.length ? nodes[0] : null;
     const values = pnode?.data;
     const mincol = sectionSize * 2 + 1;
-    const maxcol = mincol + sectionSize - 1;
+    const maxcol = mincol + sectionSize - 1 - (compare ? 1 : 0);
     const weights = _.filter(values, (v, col) => col >= mincol && col <= maxcol);
-    const warn = _.some(weights, w => formatNumeric(w) !== '100.0');
+    const warn = _.some(weights, w => isnot(w, 100)) || (compare && isnot(_.last(values), 0));
 
     return <HiGrid columnHeaders={columnHeaders}
         sectionHeaders={['Current Targets (%)', 'Trades (%)', 'Final Weights (%)']}
